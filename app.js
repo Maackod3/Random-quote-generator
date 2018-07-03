32a16742205a4164b55ba5acc24ac37ea2ac933d
Isaac Barry <isaacbarry@MacBook-Air-de-Isaac.local>
2018-07-03 03:30:25 +0200

    Little modification

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ var colors = ['#2E0927', '#D90000', '#FF2D00', '#FF8C00', '#04756F'];
 
 const url = 'https://talaikis.com/api/quotes/random/';
 
-function inIframe () { try { return window.self !== window.top; } catch (e) { return true; } }
+function inIframe() { 
+  try { 
+    return window.self !== window.top;
+  } catch(e) { 
+      return true;
+    }
+}
 
 let currentQuote = '';
 let currentAuthor = '';
-function openURL(url){
+function openURL(url) {
   window.open(url, 'Share', 'width=550, height=400, toolbar=0, scrollbars=1 ,location=0 ,statusbar=0,menubar=0, resizable=0');
 }
 function getQuote() {
